cf649e947a479a21054ad9dc81490dc3cd6ec792
pragasitc <p.charoenraksa@gmail.com>
2022-08-27 11:48:57 +0700

    Create Hello.go
    Add hello.go and how to run.

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main // Package name
+
+import "fmt" // Import starndard lib to format output data
+
+func main() { // mainfunction
+	fmt.Println("Hello World!") // use fmt to write HELLO world to console
+}
+
+/*
+- To run this peice use terminal
+- cd to hello.go
+- run $go run hello.go
+*/
