c4c9cac5481e85c274272dbb5aaaa3fee36a9ac4
pragasitc <p.charoenraksa@gmail.com>
2022-08-27 12:19:03 +0700

    Create fundamental.go
    Add a lilbit of array instruction.

---
+++ fundamental.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+	/*    ARRAY   */
+
+	//Declare array with five elements wth values.
+	array := [3]int{1, 2, 3}
+
+	//Capacity based on its init val.
+	array2 := [...]int{1, 2, 3}
+
+	// Specific assign val [0]=0 | [1]=2 | [3]:2
+	array3 := [3]int{1: 2, 2: 3}
+
+	// Change val
+	array[2] = 20
+
+	/*    ARRAY   */
+
+}
